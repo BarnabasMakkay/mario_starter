4566c825909964b926e8e79030b33d4ee1f9ee6a
Barnabas Makkay <makkaybarnabas@gmail.com>
2018-02-27 22:00:07 +0000

    Fireball now kills the enemy it collides with.

--- Assets/Scripts/FireballMovement.cs
+++ Assets/Scripts/FireballMovement.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 public class FireballMovement : MonoBehaviour {
 
     public float lifeTime;
-    public float velocityX;
-    public float velocityY;
+    public Vector3 velocity;
 
-    Rigidbody rb;
-    Vector3 velocity;
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(velocityX, velocityY, 0);
-
+        //velocity = rb.velocity;
+        rb.velocity = velocity;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (rb.velocity.y < velocityY)
+        if (rb.velocity.y < velocity.y)
         {
-            rb.velocity.Set(velocityX, velocityY, 0);
+            rb.velocity = velocity;
+        }
+        
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime < 0)
+        {
+            Destroy(gameObject);
         }
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
-        rb.velocity = new Vector3(velocityX, -velocityY, 0);
+        // if colliding with enemy
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // kill enemy
+            Destroy(collision.gameObject);
+            Debug.Log("killed enemy?");
+        }
+        else
+        {
+            rb.velocity = new Vector3(velocity.x, -velocity.y, 0);
+        }
     }
 }
